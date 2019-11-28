b13db400234cf88910b6532e8f42d6afc7ec990d
Emad <sudani.emad@gmail.com>
2019-11-28 15:53:42 +0330

    ADD: about and help

--- src/HelpMenu.java
+++ src/HelpMenu.java
@@ -18,6 +18,13 @@ public class HelpMenu extends JMenu {
 		
 		about = new JMenuItem("About Me");
 		about.setMnemonic(KeyEvent.VK_A);
+		about.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent eve) {
+				JOptionPane.showMessageDialog(null, "Powerd By: Emad", "About Me", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
 		this.add(about);
 	
 		
@@ -34,6 +41,13 @@ public class HelpMenu extends JMenu {
 		
 		help = new JMenuItem("Help");
 		help.setMnemonic(KeyEvent.VK_H);
+		help.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent eve) {
+				JOptionPane.showMessageDialog(null, "Let the second phase finished!", "Help", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
 		this.add(help);
 		
 	}
